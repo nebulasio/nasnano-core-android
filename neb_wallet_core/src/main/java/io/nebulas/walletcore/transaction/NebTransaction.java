0407d888ed8a8859c13c6c2e48d6e346d769bfc9
ping <ping@nebulas.io>
2018-10-29 13:55:07 +0800

    添加测试方法

--- neb_wallet_core/src/main/java/io/nebulas/walletcore/transaction/NebTransaction.java
+++ neb_wallet_core/src/main/java/io/nebulas/walletcore/transaction/NebTransaction.java
@@ -2,13 +2,16 @@ package io.nebulas.walletcore.transaction;
 
 import java.math.BigDecimal;
 
-public class NebTransaction<T> {
+public class NebTransaction {
     public int chainID;
     public String from;
     public String to;
     public BigDecimal value;
     public long nonce;
-    public T data;
+    /**
+     * NebBinaryData 或者 NebCallData 或者 NebDeployData
+     */
+    public Object data;
     public BigDecimal gasPrice;
     public BigDecimal gasLimit;
 }
